f85a5ab323adf89ce36e6e4ca3f2a8486f085fce
willian <wruizza@cesde.net>
2022-12-07 11:09:06 -0500

    Se crea primer controlador MEodo POST para category

---
+++ POST.Api/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POS.Aplication.Interfaces;
+using POS.Infrastructure.Commons.Bases.Request;
+
+namespace POS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryAplication _categoryApplication;
+
+        public CategoryController(ICategoryAplication categoryApplication)
+        {
+            _categoryApplication = categoryApplication;
+        }
+        [HttpPost]
+        public async Task<IActionResult> ListCategories([FromBody] BaseFiltersRequest filters)
+        {
+            var response = await _categoryApplication.ListCategories(filters);
+            return Ok(response);
+
+        }
+
+    }
+
+
+}
---
+++ POST.Aplication/Extensions/InjectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using POS.Aplication.Interfaces;
+using POS.Aplication.Services;
+using System.Reflection;
+
+namespace POS.Aplication.Extensions
+{
+    public static class InjectionExtensions
+    {
+        public static IServiceCollection AddInjectionApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddSingleton(configuration);
+
+            services.AddFluentValidation(options =>
+            {
+                options.RegisterValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies().Where(p => !p.IsDynamic));
+
+            });
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddScoped<ICategoryAplication, CategoryApplication>();
+            return services;
+        }
+    }
+}
